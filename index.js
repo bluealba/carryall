c15407083f67095a52b8f0bcf1b27834c480bf76
Claudio Fernandez <claudio.fernandez@bluealba.com>
2018-12-03 14:48:58 -0300

    Carryall

--- index.js
+++ index.js
@@ -3,14 +3,5 @@
 const Carryall = require("./lib/Carryall");
 const fs = require("fs");
 
-const Slack = require("slack");
-const token = "SQm9TXBTZRkdrk0LX6fFS9Tu"
-
-const slack = new Slack({ token });
-slack.chat.postMessage({
-	channel: "@cf",
-	text: "hola"
-})
-
-// const config = JSON.parse(fs.readFileSync("./carryall.json", { "encoding": "UTF-8" }));
-// new Carryall().deploy(config)
+const config = JSON.parse(fs.readFileSync("./carryall.json", { "encoding": "UTF-8" }));
+new Carryall().deploy(config)
