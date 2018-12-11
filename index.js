6920617b79eb1a29577996665d7f90722c7e216b
Claudio Fernandez <claudio.fernandez@bluealba.com>
2018-12-11 11:10:05 -0300

    Add batch control

--- index.js
+++ index.js
@@ -3,6 +3,20 @@
 
 const Carryall = require("./lib/Carryall");
 const fs = require("fs");
+const program = require("commander");
+const ownVersion = require("./package.json");
 
-const config = JSON.parse(fs.readFileSync("./carryall.json", { "encoding": "UTF-8" }));
+program
+	.version(ownVersion.version)
+	.option("-c", "--config <path>", "Change the configuration file. Defaults to carryall.json")
+	.option("-s, --silent", "Silent mode, will not prompt for any action")
+	.option("-R, --no-restart", "Do not perform a service restart")
+	.parse(process.argv);
+
+const config = JSON.parse(fs.readFileSync(program.config || "./carryall.json", { "encoding": "UTF-8" }));
+config.control = {
+	silent: program.silent,
+	noRestart: program.noRestart,
+}
+
 new Carryall().deploy(config)
