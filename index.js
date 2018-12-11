724d58c788eb86a9402cd23ed731f5a519d625e3
Claudio Fernandez <claudio.fernandez@bluealba.com>
2018-12-11 15:38:43 -0300

    Add list command

--- index.js
+++ index.js
@@ -6,17 +6,39 @@ const fs = require("fs");
 const program = require("commander");
 const ownVersion = require("./package.json");
 
-program
-	.version(ownVersion.version)
-	.option("-c", "--config <path>", "Change the configuration file. Defaults to carryall.json")
-	.option("-s, --silent", "Silent mode, will not prompt for any action")
-	.option("-R, --no-restart", "Do not perform a service restart")
-	.parse(process.argv);
-
-const config = JSON.parse(fs.readFileSync(program.config || "./carryall.json", { "encoding": "UTF-8" }));
-config.control = {
-	silent: program.silent,
-	noRestart: !program.restart,
+const parseConfig = (program, defaultReporter) => {
+	const config = JSON.parse(fs.readFileSync(program.config || "./carryall.json", { "encoding": "UTF-8" }));
+	config.reporter.mode = program.reporter || defaultReporter;
+	config.control = { silent: !!program.silent, noRestart: !program.restart }
+	return config;
 }
 
-new Carryall().deploy(config)
+program
+	.version(ownVersion.version)
+	.option("-c", "--config <path>", "Change the configuration file. Defaults to carryall.json");
+
+program
+	.command("deploy")
+	.description("Updates the environment installation to match versions in the descriptor")
+	.option("--reporter <reporter>", "Which reporter to use [cli|slack|combined]. Defaults to combined")
+	.option("-s, --silent", "Silent mode, will not prompt for any action")
+	.option("-R, --no-restart", "Do not perform a service restart")
+	.action(program => {
+		const config = parseConfig(program, "combined");
+		new Carryall().deploy(config)
+	});
+
+program
+	.command("list")
+	.description("Query the current state of the environment")
+	.option("--reporter <reporter>", "Which reporter to use [cli|slack|combined]. Defaults to cli")
+	.action(program => {
+		const config = parseConfig(program, "cli");
+		new Carryall().state(config)
+	});
+
+program.parse(process.argv)
+
+
+
+
