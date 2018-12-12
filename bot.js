641b066357c2570a0a75ad52581d7add0bc95103
Claudio Fernandez <claudio.fernandez@bluealba.com>
2018-12-12 13:47:16 -0300

    Add error handling

--- bot.js
+++ bot.js
@@ -1,3 +1,19 @@
 "use strict";
 
+const { WebClient } = require("@slack/client");
 const { RTMClient } = require("@slack/client");
+const { find, propEq, prop } = require("ramda");
+
+const web = new WebClient("xoxb-116831015569-493822481716-b2iWdHVRCOJ6N2NpctkblTJ4")
+const rtm = new RTMClient("xoxb-116831015569-493822481716-b2iWdHVRCOJ6N2NpctkblTJ4")
+rtm.start();
+
+web.channels.list()
+	.then(prop("channels"))
+	.then(find(propEq("name", "test")))
+	.then(channel => rtm.sendMessage("Hello there", channel.id))
+	.then(response => console.log("message sent", response.ts));
+
+rtm.on("message", message => {
+	console.log(message);
+})
